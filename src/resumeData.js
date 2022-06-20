d497e18809616b9c2903a8dd1481929770df9c8f
Micle <104517180+miclegoden@users.noreply.github.com>
2022-06-20 07:03:37 -0700

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,165 @@
+let resumeData = {
+  "imagebaseurl": "https://miclegoden.github.io/",
+  "name": "Micle Goden",
+  "role": "Fullstack & Blockchain developer",
+  "TelegramId": "@miclegoden",
+  "skypeid": "live:.cid.9d08aec71d6488a4",
+  "roleDescription": "Have been learning lots of programing languages, Javascript, Node.js, React.js, Vue.js, Python, Express... Especically Blockchain technology. Solidity, Web3, Defi,Rust ...",
+  "socialLinks": [
+    {
+      "name": "telegram",
+      "url": "https://t.me/miclegoden",
+      "className": "fa fa-telegram"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/miclegoden",
+      "className": "fa fa-github"
+    },
+    // {
+    //   "name": "skype",
+    //   "url": "http://twitter.com/rbhatia46",
+    //   "className": "fa fa-twitter"
+    // }
+  ],
+  "aboutme": "As a Fullstack & blockchain developer, have 5 years of experience about Frontend developement with react, vue and blockchain, cryptocurrency, could provide excellent programming skills, knowledge of blockchain protocols, strong analytical abilities, and effective communication skills, knowledge how to develop and secure digital transactions by creating systems to record and store blockchain data in a way that prevents changes or hacks. My design capabilities allow for frontend development. Further, I have been involved in developing application features and interfaces and maintain client and server-side applications.",
+  "address": "Montreal QC, Canada",
+  "website": "https://miclegoden.github.io",
+  "education": [
+    {
+      "UniversityName": "Elgin Community College",
+      "specialization": "Computer Science",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2018",
+      // "Achievements": "Some Achievements"
+    },
+  ],
+  "work": [
+    {
+      "CompanyName": "Matcha",
+      "specialization": "Solidity & Web3 developer",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2022",
+      "Achievements": "Senior developer"
+    },
+    {
+      "CompanyName": "iTechArt",
+      "specialization": "Python Software Engineer",
+      "MonthOfLeaving": "Feb",
+      "YearOfLeaving": "2022",
+      "Achievements": "Software Engineer"
+    },
+    {
+      "CompanyName": "Totle",
+      "specialization": "Solidity |Web3 | Defi | Frontend developerr",
+      "MonthOfLeaving": "Nov",
+      "YearOfLeaving": "2021",
+      "Achievements": "Senior Fullstack developer"
+    },
+    {
+      "CompanyName": "TrustToken",
+      "specialization": "Solidity | Defi developer",
+      "MonthOfLeaving": "Feb",
+      "YearOfLeaving": "2021",
+      "Achievements": "Blockchain developer"
+    },
+
+    {
+      "CompanyName": "ParaSwap",
+      "specialization": "Solidity | Web3 developer",
+      "MonthOfLeaving": "Jun",
+      "YearOfLeaving": "2020",
+      "Achievements": "Middle Blockchain developer"
+    },
+    {
+      "CompanyName": "JustLiquidity ",
+      "specialization": "Blockchain & Frontend developer",
+      "MonthOfLeaving": "Sept",
+      "YearOfLeaving": "2019",
+
+    },
+    {
+      "CompanyName": "Contract Voiceflip",
+      "specialization": "Full Stack Developer",
+      "MonthOfLeaving": "Oct",
+      "YearOfLeaving": "2019",
+    }
+  ],
+  "skillsDescription": "MY SKiLLS",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Javascript"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Vuejs"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "Typescript"
+    },
+    {
+      "skillname": "Express"
+    },
+    {
+      "skillname": "Nextjs"
+    },
+    {
+      "skillname": "Angluar"
+    },
+    {
+      "skillname": "Solidity"
+    },
+    {
+      "skillname": "Rust"
+    },
+    {
+      "skillname": "Web3"
+    },
+
+  ],
+  "portfolio": [
+    {
+      "name": "Financial Dashboard1",
+      // "description": "mobileapp",
+      "imgurl": "images/portfolio/board1.png"
+    },
+    {
+      "name": "Financial Dashboard2",
+      // "description": "mobileapp",
+      "imgurl": "images/portfolio/board2.png"
+    },
+    {
+      "name": "Financial Dashboard3",
+      //  "description": "mobileapp",
+      "imgurl": "images/portfolio/board3.png"
+    },
+    {
+      "name": "Financial Dashboard4",
+      // "description": "mobileapp",
+      "imgurl": "images/portfolio/board4.png"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
